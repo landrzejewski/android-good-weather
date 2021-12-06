cb49fad241db8537d37519f0c296950d2783a8c3
landrzejewski <landrzejewski.poczta@gmail.com>
2021-12-06 14:55:45 +0100

    step

--- app/src/main/java/pl/training/goodweather/forecast/adapter/view/ForecastActivity.kt
+++ app/src/main/java/pl/training/goodweather/forecast/adapter/view/ForecastActivity.kt
@@ -4,22 +4,35 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import pl.training.goodweather.R
+import pl.training.goodweather.databinding.ActivityForecastBinding
 
 internal class ForecastActivity : AppCompatActivity() {
 
     private val viewModel: ForecastViewModel by viewModels()
+    private lateinit var binding: ActivityForecastBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_forecast)
-
-        viewModel.forecast.observe(this, ::onForecastRefreshed)
-        viewModel.refreshForecast("warsaw")
+        binding = ActivityForecastBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        bindViews()
     }
 
-    private fun onForecastRefreshed(forecast: List<DayForecastViewModel>) {
-        Log.d("###",forecast.toString())
+    private fun bindViews() {
+        viewModel.forecast.observe(this, ::updateView)
+        binding.checkButton.setOnClickListener {
+            val cityName = binding.cityNameEditText.text.toString()
+            viewModel.refreshForecast(cityName)
+        }
+    }
+
+    private fun updateView(forecast: List<DayForecastViewModel>) {
+        with(forecast.first()) {
+           // binding.iconImage.setImageDrawable(getDrawable(R.id.))
+            binding.descriptionTextView.text = description
+            binding.temperatureTextView.text = temperature
+            binding.pressureTextView.text = pressure
+        }
     }
 
 }
