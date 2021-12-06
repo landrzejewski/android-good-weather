98b47e7430fbc17b951388e9b15682119f71a1bb
landrzejewski <landrzejewski.poczta@gmail.com>
2021-12-06 14:42:06 +0100

    step

--- app/src/main/java/pl/training/goodweather/forecast/adapter/view/ForecastActivity.kt
+++ app/src/main/java/pl/training/goodweather/forecast/adapter/view/ForecastActivity.kt
@@ -1,20 +1,25 @@
 package pl.training.goodweather.forecast.adapter.view
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
+import android.util.Log
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import pl.training.goodweather.R
-import pl.training.goodweather.forecast.adapter.provider.FakeForecastProvider
-import pl.training.goodweather.forecast.model.ForecastService
 
 internal class ForecastActivity : AppCompatActivity() {
 
+    private val viewModel: ForecastViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        findViewById<TextView>(R.id.text).text = ""//forecastService.getForecast("warsaw")[0].description
+        setContentView(R.layout.activity_forecast)
+
+        viewModel.forecast.observe(this, ::onForecastRefreshed)
+        viewModel.refreshForecast("warsaw")
+    }
+
+    private fun onForecastRefreshed(forecast: List<DayForecastViewModel>) {
+        Log.d("###",forecast.toString())
     }
 
 }
--- app/src/main/java/pl/training/goodweather/forecast/adapter/view/ForecastViewModel.kt
+++ app/src/main/java/pl/training/goodweather/forecast/adapter/view/ForecastViewModel.kt
@@ -5,6 +5,9 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
+import pl.training.goodweather.commons.formatDate
+import pl.training.goodweather.commons.formatPressure
+import pl.training.goodweather.commons.formatTemperature
 import pl.training.goodweather.forecast.adapter.provider.FakeForecastProvider
 import pl.training.goodweather.forecast.model.DayForecast
 import pl.training.goodweather.forecast.model.ForecastService
@@ -12,14 +15,18 @@ import pl.training.goodweather.forecast.model.ForecastService
 internal class ForecastViewModel : ViewModel() {
 
     private val forecastService = ForecastService(FakeForecastProvider())
-    private val forecastData = MutableLiveData<List<DayForecast>>()
+    private val forecastData = MutableLiveData<List<DayForecastViewModel>>()
 
-    val forecast: LiveData<List<DayForecast>> = forecastData
+    val forecast: LiveData<List<DayForecastViewModel>> = forecastData
 
     fun refreshForecast(city: String) {
         viewModelScope.launch {
-            forecastData.value = forecastService.getForecast(city);
+            forecastData.value = forecastService.getForecast(city).map(::toViewModel)
         }
+    }
+
+    private fun toViewModel(dayForecast: DayForecast) = with(dayForecast) {
+        DayForecastViewModel(icon, description, formatTemperature(temperature), formatPressure(pressure), formatDate(date))
     }
 
 }
