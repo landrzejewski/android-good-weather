40d9e26f1d06db435fd2cb9f27767115a11daab9
landrzejewski <landrzejewski.poczta@gmail.com>
2021-12-09 10:01:28 +0100

    step

--- app/src/main/java/pl/training/goodweather/MainActivity.kt
+++ app/src/main/java/pl/training/goodweather/MainActivity.kt
@@ -2,14 +2,18 @@ package pl.training.goodweather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import pl.training.goodweather.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
+    //private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,13 +22,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         setSupportActionBar(binding.mainToolbar)
         val navController = findNavController(R.id.mainContentFragment)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        //appBarConfiguration = AppBarConfiguration(navController.graph)
+        //setupActionBarWithNavController(navController, appBarConfiguration)
+        val bottomNavigationBar = findViewById<BottomNavigationView>(R.id.tabsBottomNavigationView)
+        bottomNavigationBar.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.mainContentFragment)
         return navController.navigateUp() || super.onSupportNavigateUp();
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.settingsFragment -> {
+                findNavController(R.id.mainContentFragment).navigate(R.id.settingsFragment)
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
 }
---
+++ app/src/main/java/pl/training/goodweather/profile/adapter/view/ProfileViewModel.kt
@@ -0,0 +1,6 @@
+package pl.training.goodweather.profile.adapter.view
+
+import androidx.lifecycle.ViewModel
+
+class ProfileViewModel : ViewModel() {
+}
